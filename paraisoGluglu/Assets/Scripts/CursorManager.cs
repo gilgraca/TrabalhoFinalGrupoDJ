53dad82337f264f891706109a0deb28cd9833fcf
Gil Graça <84124194+gilgraca@users.noreply.github.com>
2025-06-17 12:04:11 +0100

    17/06
    CursorManager

---
+++ paraisoGluglu/Assets/Scripts/CursorManager.cs
@@ -0,0 +1,26 @@
+
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    // Define se o cursor deve estar visível (true = menus, false = gameplay)
+    [SerializeField] private bool mostrarCursor = false;
+
+    void Start()
+    {
+        if (mostrarCursor)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+
+            Debug.Log("Cursor visível e desbloqueado (menu).");
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+
+            Debug.Log("Cursor escondido e bloqueado (nível).");
+        }
+    }
+}
