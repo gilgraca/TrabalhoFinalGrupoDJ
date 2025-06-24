82b46d9932d081bcf1ac0330d8b3231b936bf8e4
pt1C3 <100787143+pt1C3@users.noreply.github.com>
2025-06-24 17:27:44 +0100

    falta mais modelos e inimigos, mas de resto ta tudo

--- paraisoGluglu/Assets/Scripts/SpawnerDeItens.cs
+++ paraisoGluglu/Assets/Scripts/SpawnerDeItens.cs
@@ -28,7 +28,7 @@ public class SpawnerDeItens : MonoBehaviour
         }
 
         // Escolhe aleatoriamente onde vai aparecer a chave
-        int indiceChave = Random.Range(4, pontosDeSpawn.Length);
+        int indiceChave = Random.Range(5, pontosDeSpawn.Length); //os primerios 5 milhos são no corredor
 
         for (int i = 0; i < pontosDeSpawn.Length; i++)
         {
